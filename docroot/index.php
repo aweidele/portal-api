84224369bf6730045346c4ab4677006c58fad3a5
Aaron Weidele <aweidele@gmail.com>
2025-02-18 15:11:42 -0500

    adding the query again

--- docroot/index.php
+++ docroot/index.php
@@ -12,11 +12,11 @@ if (file_exists($config)) {
 	die('Both main and fallback files are missing.');
 }
 
-// $sql = "
-// 	SELECT		*
-// 	FROM		links, link_cat
-// 	WHERE		active = 1 AND catID = cat
-// 	ORDER BY	rank, linkName";
-// $sql_result = mysqli_query($connection, $sql) or die ("Couldn't execute query.");
-// $results = mysqli_fetch_all($sql_result, MYSQLI_ASSOC);
-// echo json_encode($results);
+$sql = "
+	SELECT		*
+	FROM		links, link_cat
+	WHERE		active = 1 AND catID = cat
+	ORDER BY	rank, linkName";
+$sql_result = mysqli_query($connection, $sql) or die ("Couldn't execute query.");
+$results = mysqli_fetch_all($sql_result, MYSQLI_ASSOC);
+echo json_encode($results);
