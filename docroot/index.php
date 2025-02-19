c6a0a3cf9bac3904ec4ca0c0975689d4c87dd46d
Aaron Weidele <aweidele@gmail.com>
2025-02-19 10:40:01 -0500

    Refactoring

--- docroot/index.php
+++ docroot/index.php
@@ -3,6 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json; charset=utf-8');
 $config = "config.php";
 $configDefault = "config_default.php";
+$output = [];
 
 if (file_exists($config)) {
 	require $config;
@@ -12,18 +13,38 @@ if (file_exists($config)) {
 	die('Both main and fallback files are missing.');
 }
 
-$portalID = $_GET["portalID"];
-
-$sql = "SELECT * FROM links, link_cat ";
-$sql .= "WHERE active = 1 AND catID = cat ";
-if($portalID) { 
-	$sql .= "AND links.portal = \"".$portalID."\" "; 
-}
-$sql .= "ORDER BY rank, linkName";
-
 $connection = mysqli_connect($server, $user, $password) or die ("Couldn't connect to server.");
 $db = mysqli_select_db($connection,$database) or die ("Couldn't select database");
 
+$portalID = $_GET["portalID"];
+
+/* GET BOOKMARKS */
+$sql = "SELECT * FROM links ";
+$sql .= "WHERE active = 1 ";
+if($portalID) { 
+	$sql .= "AND portal = '".$portalID."' "; 
+}
+$sql .= "ORDER BY linkName";
+
 $sql_result = mysqli_query($connection, $sql) or die ("Couldn't execute query.");
 $results = mysqli_fetch_all($sql_result, MYSQLI_ASSOC);
-echo json_encode($results);
+
+$output['bookmarks'] = [
+	'results' => $results,
+	'query'=>$sql
+];
+
+/* GET LINK CATEGORIES */
+$sql = "SELECT * FROM link_cat ";
+if($portalID) {
+	$sql .= "WHERE portal = '".$portalID."' ";
+}
+$sql .= "ORDER BY rank";
+$sql_result = mysqli_query($connection, $sql) or die ("Couldn't execute query.");
+$results = mysqli_fetch_all($sql_result, MYSQLI_ASSOC);
+
+$output['categories'] = [
+	'results' => $results,
+	'query'=>$sql
+];
+echo json_encode($output);
