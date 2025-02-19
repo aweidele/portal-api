0f368e4b7a1eba55bcdcfb6e2215563fc8660725
AARON WEIDELE <>
2025-02-18 20:24:33 -0500

    Removing extra stuff

--- docroot/index.php
+++ docroot/index.php
@@ -21,19 +21,9 @@ if($portalID) {
 }
 $sql .= "ORDER BY rank, linkName";
 
-// SELECT * FROM links, link_cat WHERE active = 1 AND catID = cat AND links.portal = "b8aee059" ORDER BY rank, linkName;
-
-// $sql = "
-// 	SELECT		*
-// 	FROM		links, link_cat
-// 	WHERE		active = 1 AND catID = cat
-// 	ORDER BY	rank, linkName";
-
 $connection = mysqli_connect($server, $user, $password) or die ("Couldn't connect to server.");
 $db = mysqli_select_db($connection,$database) or die ("Couldn't select database");
 
 $sql_result = mysqli_query($connection, $sql) or die ("Couldn't execute query.");
 $results = mysqli_fetch_all($sql_result, MYSQLI_ASSOC);
-$results[] = $portalID;
-$results[] = $sql;
 echo json_encode($results);
