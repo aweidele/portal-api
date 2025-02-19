2a87f94c2e2b9b508fec58e3cbe7a1f9608ae60d
AARON WEIDELE <>
2025-02-18 20:13:57 -0500

    test sql

--- docroot/index.php
+++ docroot/index.php
@@ -27,12 +27,12 @@ $sql .= "ORDER BY rank, linkName";
 // 	WHERE		active = 1 AND catID = cat
 // 	ORDER BY	rank, linkName";
 
-	// echo $sql;
-$connection = mysqli_connect($server, $user, $password) or die ("Couldn't connect to server.");
-$db = mysqli_select_db($connection,$database) or die ("Couldn't select database");
+	echo $sql;
+// $connection = mysqli_connect($server, $user, $password) or die ("Couldn't connect to server.");
+// $db = mysqli_select_db($connection,$database) or die ("Couldn't select database");
 
-$sql_result = mysqli_query($connection, $sql) or die ("Couldn't execute query.");
-$results = mysqli_fetch_all($sql_result, MYSQLI_ASSOC);
-$results[] = $portalID;
-$results[] = $sql;
-echo json_encode($results);
+// $sql_result = mysqli_query($connection, $sql) or die ("Couldn't execute query.");
+// $results = mysqli_fetch_all($sql_result, MYSQLI_ASSOC);
+// $results[] = $portalID;
+// $results[] = $sql;
+// echo json_encode($results);
