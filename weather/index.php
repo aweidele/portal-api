7519c27ff0baf0b9dcef90c8ca73414e1d278c20
Aaron Weidele <aaron.weidele@aristotle.com>
2025-11-05 16:40:02 -0500

    adding weather endpoint

---
+++ weather/index.php
@@ -0,0 +1,47 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
+
+function getWeatherData() {
+    $filename = "weather-data.json";
+    $now = time();
+    
+    $saved_json = file_get_contents($filename);
+    $saved = json_decode($saved_json);
+    $dif = $now - $saved->now;
+    
+    if($dif < 90) {
+        return $saved;
+    }
+    
+    $url = "https://api.openweathermap.org/data/3.0/onecall?lat=39.268391&lon=-76.724636&appid=1d0100db75fac71d32a013ff396f1544&units=imperial";
+    $ch = curl_init();
+
+    $options = [
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true, 
+    ];
+    
+    curl_setopt_array($ch, $options);
+    $response = curl_exec($ch);
+     
+    if ($response === false) {
+        $error = curl_error($ch);
+        curl_close($ch);
+        return ["cURL Error" => $error];
+    }
+    
+    curl_close($ch);
+    
+    $data = json_decode($response);
+    $data->now = $now;
+    $data->dif = $dif;
+    
+    file_put_contents($filename, json_encode($data));
+
+    return $data;
+}
+
+
+echo json_encode(getWeatherData());
